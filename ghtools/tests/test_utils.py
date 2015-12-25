92f46587409a563c73f2e0345e5a4ef24e6d72f7
Rob van der Leek <r.vanderleek@sig.eu>
2015-12-25 23:24:02 +0100

    Fixed tests

--- ghtools/tests/test_utils.py
+++ ghtools/tests/test_utils.py
@@ -1,18 +1,8 @@
 from unittest import TestCase
-from StringIO import StringIO
 
 import ghtools.utils
 
 class utilsTest(TestCase):
-
-    def test_log(self):
-        out = StringIO()
-
-        ghtools.utils.log("Hello world!", out=out)
-        result = out.getvalue().strip()
-
-        self.assertTrue(result.endswith("Hello world!"))
-
     def test_get_all_gh_languages(self):
         langs = ghtools.utils.get_all_gh_languages()
 
